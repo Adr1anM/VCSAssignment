fa25aab6da5fbf0087dff1618479ad2ddfec21c0
Adrian <andrian.melnic2003@gmail.com>
2024-03-08 22:32:52 +0200

    Added passwordrepeat proprety

--- TestProject/User.cs
+++ TestProject/User.cs
@@ -11,7 +11,9 @@ namespace TestProject
         public int Id { get; set; }
         public string? UserName { get; set; }
         public string? Password { get; set; }   
+        public string? PasswordRepeat { get; set; } 
         public string? Email { get; set; }
+
 
     }
 }
